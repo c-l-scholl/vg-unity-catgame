8bb3c6e235494a7167d095f40acf0333f82aaa1a
Kaliana Andriamananjara <kandriam@macalester.edu>
2024-02-22 10:10:17 -0600

    Update NpcScripts
    Created NpcScripts relationship and hatesCat vars and methods

--- Assets/Scripts/NpcScripts.cs
+++ Assets/Scripts/NpcScripts.cs
@@ -6,13 +6,15 @@ public class NpcScripts : MonoBehaviour
 {
     string name;
     string questObject;
+    bool hatesCat;
+    int relationship;
 
     // Start is called before the first frame update
     void Start()
     {
         name = "";
         questObject = "";
-        
+        hatesCat = false;
     }
 
     // Sets the NPCs QuestObject name
@@ -21,6 +23,30 @@ public class NpcScripts : MonoBehaviour
         questObject = objectName;
     }
 
+    // Incrament (increase) the relationship of the NPC
+    // with the cat by a certain value i 
+    void incRelat(int i)
+    {
+        relationship += i;
+    }
+
+    // Decrament (decrease) the relationship of the NPC
+    // with the cat by a certain value i
+    // If relationship is below 0, set hatesCat to true
+    void decRelat(int i)
+    {
+        relationship -= i;
+        if (relationship <= 0)
+        {
+            setHatesCat();
+        }
+    }
+
+    // Sets an NPCs hatred of the cat to true
+    void setHatesCat(){
+        hatesCat = true; // NPC now hates cat
+
+    }
     // Update is called once per frame
     void Update()
     {
