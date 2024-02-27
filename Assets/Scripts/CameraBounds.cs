8bf6e35c7165ac8a0f4a1da52d1621544311a8fc
RousseauMalco <rousseaumalco@gmail.com>
2024-02-26 21:58:54 -0600

    Figured out camera, ATM only in workspace.

---
+++ Assets/Scripts/CameraBounds.cs
@@ -0,0 +1,114 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraBounds : MonoBehaviour
+{
+    public GameObject player;
+
+    public Vector2 maxPos;
+    public Vector2 minPos;
+    int zPos = -10;
+
+    bool withinMaxX;
+    bool withinMinX;
+    bool withinMaxY;
+    bool withinMinY;
+    bool withinX;
+    bool withinY;
+    bool playerWithin;
+
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zPos);
+    }
+
+    void Update() {
+        checkBounds();
+        if (playerWithin) {
+            transform.position = new Vector3(player.transform.position.x, player.transform.position.y, zPos);
+        } else {
+            Debug.Log("player not within");
+            Debug.Log(withinMaxX + " withinmaxX");
+            Debug.Log(withinMaxY + " withinmaxY");
+            Debug.Log(withinMinX + " withinminX");
+            Debug.Log(withinMinY + " withinminY");
+            if (!withinX && withinY) {
+                if (!withinMaxX) {
+                    transform.position = new Vector3(maxPos.x, player.transform.position.y, zPos);
+                } else {
+                    transform.position = new Vector3(minPos.x, player.transform.position.y, zPos);
+                }
+            }
+
+            if (withinX && !withinY) {
+                if (!withinMaxY) {
+                    transform.position = new Vector3(player.transform.position.x, maxPos.y, zPos);
+                } else {
+                    transform.position = new Vector3(player.transform.position.x, minPos.y, zPos);
+                }
+            }
+
+            if (!withinX && !withinY) {
+                if (!withinMaxX && !withinMaxY) {
+                    transform.position = new Vector3(maxPos.x, maxPos.y, zPos);
+                } else if (!withinMaxX && !withinMinY) {
+                    transform.position = new Vector3(maxPos.x, minPos.y, zPos);
+                } else if (!withinMinX && !withinMaxY) {
+                    transform.position = new Vector3(minPos.x, maxPos.y, zPos);
+                } else if (!withinMinX && !withinMinY) {
+                    transform.position = new Vector3(minPos.x, minPos.y, zPos);
+                }
+            }
+        }
+    }   
+
+    void checkBounds() {
+        if (player.transform.position.x > maxPos.x) {
+            withinMaxX = false;
+        } else {
+            withinMaxX = true;
+        }
+
+        if (player.transform.position.x < minPos.x) {
+            withinMinX = false;
+        } else {
+            withinMinX = true;
+        }
+
+        if (player.transform.position.y > maxPos.y) {
+            withinMaxY = false;
+        } else {
+            withinMaxY = true;
+        }
+
+        if (player.transform.position.y < minPos.y) {
+            withinMinY = false;
+        } else {
+            withinMinY = true;
+        }
+
+        if (withinMaxX && withinMinX) {
+            withinX = true;
+        } else {
+            withinX = false;
+        }
+
+        if (withinMaxY && withinMinY) {
+            withinY = true;
+        } else {
+            withinY = false;
+        }
+
+        if (player.transform.position.x < maxPos.x && player.transform.position.x > minPos.x && 
+        player.transform.position.y < maxPos.y && player.transform.position.y > minPos.y) {
+            playerWithin = true;
+            Debug.Log("Within");
+        } else {
+            playerWithin = false;
+            Debug.Log("Not within" + playerWithin);
+        }
+    }
+}
