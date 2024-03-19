5c179d3ea54a74ee06f8ed12424666f5682b7f60
RousseauMalco <rousseaumalco@gmail.com>
2024-03-19 11:22:20 -0500

    Added buttons to interacting with box.

--- Assets/Scripts/Box Scripts/BoxInteraction.cs
+++ Assets/Scripts/Box Scripts/BoxInteraction.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements.Experimental;
 
 public class BoxInteraction : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private bool interacting;
+    public GameObject boxUI;
+
     void Start()
     {
-        
+        interacting = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D other)
     {
-        
+        if (Input.GetKey(KeyCode.Space) && !interacting)
+        {
+            if (other.TryGetComponent(out CatMovement movement)) {
+                movement.enabled = false;
+            }
+            interacting = true;
+            boxUI.GetComponent<Canvas>().enabled = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out CatMovement movement)) {
+            movement.enabled = true;
+        }
+        interacting = false;
+        boxUI.GetComponent<Canvas>().enabled = false;
     }
 }
