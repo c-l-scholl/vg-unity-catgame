97a764bb50a587c4026cce1fcc086a763ae1494a
Kaliana Andriamananjara <kandriam@macalester.edu>
2024-03-28 10:41:16 -0500

    LittleGirlQuest and npcScripts
    Modified LittleGirlQuest to have dialogue and removed NptScripts

--- Assets/Scripts/LittleGirlQuest.cs
+++ Assets/Scripts/LittleGirlQuest.cs
@@ -9,18 +9,25 @@ public class LittleGirlQuest : MonoBehaviour
 	public GameObject L2; // or the rest of the map or something
 	public Inventory catInventory;
 
+    public DialogueManager dialogueManager;
+
+	public DialogueTree questIncomplete;
+	public DialogueTree questCompleted;
+	public DialogueTree questItemAcquired;
+	// public class DialogueTree hatesCat;
+
 
 	private enum QuestProgress
 	{
-		HAVENT_MET,
-		ITEM_REQUESTED,
+		QUEST_INCOMPLETE, // Default, hasn't given item to npc, hasn't been acquired
+		QUEST_ITEM_ACQUIRED, // player has item
 		QUEST_COMPLETE // item has been given
-
-	}
+		// HATES_CAT // Player hates
+	};
 	private QuestProgress currentQuestProg;
 	void Start()
 	{
-		currentQuestProg = QuestProgress.HAVENT_MET;
+		currentQuestProg = QuestProgress.QUEST_INCOMPLETE;
 	}
 
 	public void AdvanceQuest() // needs access to Player's inventory
@@ -28,34 +35,24 @@ public class LittleGirlQuest : MonoBehaviour
 		// state machine based on quest
 		switch (currentQuestProg)
 		{
-			case QuestProgress.HAVENT_MET:
+			case QuestProgress.QUEST_INCOMPLETE:
 				// initial dialogue
-				if (catInventory.RemoveItemFromInventory(questItem))
+				if (catInventory.RemoveItemFromInventory(questItem)) // Player has item
 				{
 					currentQuestProg = QuestProgress.QUEST_COMPLETE;
 					// dialogue
+					dialogueManager.StartDialogue(questItemAcquired);
 				}
-				else
+				else // Player doesn't have item
 				{
-					currentQuestProg = QuestProgress.ITEM_REQUESTED;
-					// dialogue
+					// dialogue 
+					dialogueManager.StartDialogue(questIncomplete);
 				}
 				this.AdvanceQuest();
 				break;
-			case QuestProgress.ITEM_REQUESTED:
-				if (catInventory.RemoveItemFromInventory(questItem))
-				{
-					currentQuestProg = QuestProgress.QUEST_COMPLETE;
-					// dialogue
-					this.AdvanceQuest();
-				}
-				else
-				{
-					// reminder dialogue
-				}
-				break;
 			case QuestProgress.QUEST_COMPLETE:
 				// basic dialogue
+				dialogueManager.StartDialogue(questCompleted);
 				break;
 		}
 	}
