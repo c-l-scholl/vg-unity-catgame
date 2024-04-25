a0dcfb3486233a6575c7257844f2ac81a44e7c33
c-l-scholl <cscholl@macalester.edu>
2024-04-25 11:13:48 -0500

    added foodspawners for section 1 in CW

---
+++ Assets/Scripts/FoodSpawner/FoodSpawner.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class FoodSpawner : MonoBehaviour
+{
+
+    [Serializable]
+    public struct Location
+    {
+        public int TOTAL_FOOD;
+        public GameObject mapSection;
+        public Transform[] spawnPositions;
+        public Transform GetSpawnPosition()
+        {
+            int i = UnityEngine.Random.Range(0, spawnPositions.Length);
+            return spawnPositions[i];
+        }
+        public bool IsMapActive()
+        {
+            return mapSection.activeSelf;
+        }
+    }
+    [SerializeField]
+    private InventoryItemData[] foodItems;
+    public Location[] locations;
+
+    [SerializeField]
+    private int numFoodPerDay;
+
+    public void SpawnFood()
+    {
+        foreach (Location location in locations)
+        {
+            if (!location.IsMapActive())
+            {
+                continue;
+            }
+            for (int i = 0; i < numFoodPerDay; i++)
+            {
+                GameObject foodToSpawn = foodItems[UnityEngine.Random.Range(0, foodItems.Length)].model;
+                Instantiate(foodToSpawn, location.GetSpawnPosition());
+            }
+        }
+    }
+}
