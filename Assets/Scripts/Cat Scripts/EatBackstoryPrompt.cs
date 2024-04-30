7730fa69c6b600b72f0ae428d98bb1222aed93b4
c-l-scholl <cscholl@macalester.edu>
2024-04-30 14:21:13 -0500

    added script to scene

---
+++ Assets/Scripts/Cat Scripts/EatBackstoryPrompt.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatBackstoryPrompt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void RandomChance(int probability)
+}
