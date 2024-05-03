d2075de799055ef38ee7739c744baa00fc94dd13
c-l-scholl <cscholl@macalester.edu>
2024-05-02 19:27:46 -0500

    fixed bagui not disappering when getting hit by a car

--- Assets/Scripts/Box Scripts/UIInventory.cs
+++ Assets/Scripts/Box Scripts/UIInventory.cs
@@ -13,46 +13,58 @@ public class UIInventory : MonoBehaviour
     private int emptySlots;
     private Vector2 dropLocation;
 
-    private void Awake() {
+    private void Awake()
+    {
         ResetInventory();
     }
 
-    public void ResetInventory() {
+    public void ResetInventory()
+    {
         uiItems.Clear();
-        for (int i = 0; i < numberOfSlots; i ++) {
+        for (int i = 0; i < numberOfSlots; i++)
+        {
             GameObject instance = Instantiate(slotPrefab);
             instance.transform.SetParent(slotPanel);
             uiItems.Add(instance.GetComponentInChildren<UIItem>());
         }
         emptySlots = numberOfSlots;
     }
-   
-    public void UpdateSlot(int slot, InventoryItemData item) {
+
+    public void UpdateSlot(int slot, InventoryItemData item)
+    {
         uiItems[slot].UpdateItem(item);
     }
 
-    public bool AddNewItem(InventoryItemData item) {
+    public bool AddNewItem(InventoryItemData item)
+    {
         UpdateSlot(uiItems.FindIndex(i => i.item == null), item);
         bool result = inventory.AddItemToInventory(item);
-        if (!result) {
+        if (!result)
+        {
             Debug.Log("Add item failed");
         }
         emptySlots--;
         return result;
     }
 
-    public void RemoveItem(InventoryItemData item) {
+    public void RemoveItem(InventoryItemData item)
+    {
         UpdateSlot(uiItems.FindIndex(i => i.item == item), null);
         inventory.RemoveItemFromInventory(item);
         emptySlots++;
     }
 
-    public void DropItem(int index) {
-        if (uiItems[index].item != null) {
+    public void DropItem(int index)
+    {
+        if (uiItems[index].item != null)
+        {
 
-            if (numberOfSlots == 3) {
-                dropLocation = new Vector2(dropLocationAnchor.transform.position.x, dropLocationAnchor.transform.position.y-2);
-            } else {
+            if (numberOfSlots == 3)
+            {
+                dropLocation = new Vector2(dropLocationAnchor.transform.position.x, dropLocationAnchor.transform.position.y - 2);
+            }
+            else
+            {
                 dropLocation = new Vector2(dropLocationAnchor.transform.position.x, dropLocationAnchor.transform.position.y);
             }
 
@@ -63,9 +75,11 @@ public class UIInventory : MonoBehaviour
             emptySlots++;
         }
     }
-    
-    public InventoryItemData RemoveItemReturn(int index) {
-        if (uiItems[index].item != null) {
+
+    public InventoryItemData RemoveItemReturn(int index)
+    {
+        if (uiItems[index].item != null)
+        {
 
             InventoryItemData item = uiItems[index].item;
             inventory.RemoveItem(index);
@@ -76,9 +90,12 @@ public class UIInventory : MonoBehaviour
         return null;
     }
 
-    public void RemoveItem(int index) {
-        if (uiItems.Count > 0) {
-            if (uiItems[index].item != null) {
+    public void RemoveItem(int index)
+    {
+        if (uiItems.Count > 0)
+        {
+            if (uiItems[index].item != null)
+            {
 
                 inventory.RemoveItem(index);
                 uiItems[index].UpdateItem(null);
